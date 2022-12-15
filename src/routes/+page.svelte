7eba8f50735db16f525ef4023fd046b569289f8d
Nate Otto <nate@ottonomy.net>
2022-12-15 11:04:59 -0800

    Add error message for improper environment config.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { error } from '@sveltejs/kit';
 	import PublisherConfig from '$lib/partials/PublisherConfig.svelte';
 	import BadgeSourceConfig from '$lib/partials/BadgeSourceConfig.svelte';
 	import DataProofing from '$lib/partials/DataProofing.svelte';
@@ -7,7 +8,16 @@
 	import NextPrevButton from '$lib/components/NextPrevButton.svelte';
 
 	import BodyText from '$lib/components/typography/BodyText.svelte';
-</script>
+
+	import {
+		PUBLIC_UI_API_BASEURL,
+		PUBLIC_PUBLISHER_API_BASEURL,
+		PUBLIC_PUBLISHER_API_ENV_LABEL
+	} from '$env/static/public';
+
+	if (!PUBLIC_UI_API_BASEURL || !PUBLIC_PUBLISHER_API_BASEURL || !PUBLIC_PUBLISHER_API_ENV_LABEL)
+		throw error(404, { message: 'Application environment settings not properly configured. Follow instructions on setting up the .env'})
+	</script>
 
 <BodyText>
 	This app can help publish new and updated Open Badges to the Credential Registry. After importing
