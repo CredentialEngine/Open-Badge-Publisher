51a957405741dba025abc5c6f582265c509dda5d
Michael Parsons <58825866+mparsons-ce@users.noreply.github.com>
2024-07-08 14:57:14 -0500

    Add commands to publish to the new VM

---
+++ buildProductionNew.cmd
@@ -0,0 +1,15 @@
+cd D:\Github\Open-Badge-Publisher
+copy .env backup.env
+copy productionNew.env .env
+RD /S /Q badgePublisher_prod
+
+pause
+npm run build
+pause
+copy backup.env .env
+
+cd D:\Github\Open-Badge-Publisher
+
+REN "build" badgePublisher_prod_%date:~10,4%%date:~4,2%%date:~7,2%_%time:~0,2%%time:~3,2%
+
+pause
---
+++ buildProduction_renameNEW.cmd
@@ -0,0 +1,4 @@
+
+cd D:\Github\Open-Badge-Publisher
+
+REN "build" badgePublisher_prodNew_%date:~10,4%%date:~4,2%%date:~7,2%_%time:~0,2%%time:~3,2%
