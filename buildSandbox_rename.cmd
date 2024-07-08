a81fd8c040ed4886c48ec2e3b60ff53fad21dccf
Michael Parsons <58825866+mparsons-ce@users.noreply.github.com>
2024-07-08 15:11:02 -0500

    correct sandbox rename cmd

--- buildSandbox_rename.cmd
+++ buildSandbox_rename.cmd
@@ -1,5 +1,5 @@
 
-cd D:\azure\source\repos\Open-Badge-Publisher
+cd D:\Github\Open-Badge-Publisher
 @Echo Renaming to badgePublisher_sandbox
 REN "build" badgePublisher_sandbox_%date:~10,4%%date:~4,2%%date:~7,2%_%time:~0,2%%time:~3,2%
 REN "build" badgePublisher_sandbox_%date:~10,4%%date:~4,2%%date:~7,2%_%time:~1,1%%time:~3,2%
